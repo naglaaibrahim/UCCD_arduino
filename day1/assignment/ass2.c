bb5f8f48edef42c45f6d94ff88a87c61a29c7dac
NaglaaIbrahim <naglaa762061@std.mans.edu.eg>
2022-03-26 04:48:13 -0700

    add 2 ass

--- day1/assignment/ass2.c
+++ day1/assignment/ass2.c
@@ -1,14 +1,13 @@
-#include<stdio.h>
- void main(){
-     int number1, number2, number3;
-
-printf("please enter number1 :");
-scanf(" %d",&number1);
-printf("please enter number2 :");
-scanf(" %d",&number2);
-printf("please enter number3 :");
-scanf(" %d",&number3);
-printf("the number3 is : %d\n",number3);
-printf("the number2 is : %d\n",number2);
-printf("the number1 is : %d\n",number1);
+#include <stdio.h>
+void main() {
+	int number 1,number 1,number 3;
+	printf("please enter number 1:");
+	scanf(" %d",&number 1);
+	printf("please enter number 2:");
+	scanf(" %d",&number 2);
+	printf("please enter number 3:");
+	scanf(" %d",&number 3);
+	printf("number 3:%d\n",number 3);
+	printf("number 2:%d\n",number 2);
+	printf("number 1:%d\n",number 1);
 }
