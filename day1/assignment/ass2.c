c2a1274e980749bceef314e4bdba1a722836c0f4
NaglaaIbrahim <naglaa762061@std.mans.edu.eg>
2022-03-26 03:55:23 -0700

    day1

---
+++ day1/assignment/ass2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+ void main(){
+     int number1, number2, number3;
+
+printf("please enter number1 :");
+scanf(" %d",&number1);
+printf("please enter number2 :");
+scanf(" %d",&number2);
+printf("please enter number3 :");
+scanf(" %d",&number3);
+printf("the number3 is : %d\n",number3);
+printf("the number2 is : %d\n",number2);
+printf("the number1 is : %d\n",number1);
+}
