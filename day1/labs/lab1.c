0a5d247ac28b92b1a612ef1e973a93e21679a311
NaglaaIbrahim <naglaa762061@std.mans.edu.eg>
2022-03-27 01:58:07 -0700

    add labs

---
+++ day1/labs/lab1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void main(){
+printf("I am naglaa ibrahim\nmy birth date is 1 /1/2001\nI still studying in faculty of engineerin Mansoura University\nI will graduate in 2024");
+
+}
---
+++ day1/labs/lab2.c
@@ -0,0 +1,9 @@
+
+#include <stdio.h>
+
+void main(){
+int x;
+printf("please enter the value :");
+scanf(" %d", &x);
+printf(" the value you entered is %d:",x);
+}
