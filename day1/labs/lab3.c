8bd902853497ae065f0a5249eaf46e341b65ddaf
NaglaaIbrahim <naglaa762061@std.mans.edu.eg>
2022-03-27 02:05:01 -0700

    add labs

---
+++ day1/labs/lab3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main(){
+int num1 , num2 ;
+printf("please enter num1 :");
+scanf(" %d", &num1);
+printf("please enter num2 :");
+scanf(" %d", &num2);
+printf(" num1+num2= %d\n ",num1+num2);
+printf("num1*num2= %d",num1*num2);
+}
+
